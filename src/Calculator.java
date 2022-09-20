b97d0576861e3497be19827a00babbe9a583c0be
Ivan Kostin <kosstin@gmail.com>
2022-09-20 14:28:59 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,24 @@
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    //BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y == 0) {
+            System.out.println("Деление на ноль!");
+            return 0;
+        }
+        return x / y;
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : -x;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+
+
+}
